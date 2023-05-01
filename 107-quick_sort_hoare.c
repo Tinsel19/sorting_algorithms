eb80367decc05b923483ee15688bd89490376921
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2023-04-30 19:14:45 -0700

    Tinsel19

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,82 +1,70 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 
 /**
- * partition - finds the partition for the quicksort using the Hoare scheme
- * @array: array to sort
- * @lo: lowest index of the partition to sort
- * @hi: highest index of the partition to sort
+ * quick_sort_hoare - sorts an array with the Quicksort algorithm
+ * @array: array of ints to sort
  * @size: size of the array
- *
- * Return: index of the partition
  */
-
-int partition(int *array, ssize_t lo, ssize_t hi, size_t size)
-{
-	int swap, pivot;
-
-	pivot = array[hi];
-	while (lo <= hi)
-	{
-		while (array[lo] < pivot)
-			lo++;
-		while (array[hi] > pivot)
-			hi--;
-		if (lo <= hi)
-		{
-			if (lo != hi)
-			{
-				swap = array[lo];
-				array[lo] = array[hi];
-				array[hi] = swap;
-				print_array(array, size);
-			}
-			lo++;
-			hi--;
-		}
-	}
-	return (hi);
-}
-
-
-/**
- * quicksort - sorts a partition of an array of integers
- * @array: array to sort
- * @lo: lowest index of the partition to sort
- * @hi: highest index of the partition to sort
- * @size: size of the array
- *
- * Return: void
- */
-
-void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
-{
-	ssize_t pivot;
-
-	if (lo < hi)
-	{
-		pivot = partition(array, lo, hi, size);
-		quicksort(array, lo, pivot, size);
-		quicksort(array, pivot + 1, hi, size);
-
-	}
-}
-
-
-/**
- * quick_sort_hoare - sorts an array of integers in ascending order using the
- * Quick sort algorithm
- * @array: The array to sort
- * @size: The size of the array
- *
- * Return: void
- */
-
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
-	quicksort(array, 0, size - 1, size);
+
+	quick_recursion(array, 0, (int)size - 1, size);
+}
+
+/**
+ * quick_recursion - helper function for Quicksort
+ * @array: array to sort
+ * @left: index of the left element
+ * @right: index of the right element
+ * @size: size of the array
+ */
+void quick_recursion(int *array, int left, int right, size_t size)
+{
+	int piv;
+
+	if (left < right)
+	{
+		piv = partition(array, left, right, size);
+		quick_recursion(array, left, piv - 1, size);
+		quick_recursion(array, piv, right, size);
+	}
+}
+
+/**
+ * partition - gives a piv index for Quicksort
+ * @array: array to find the piv in
+ * @left: index of the left element
+ * @right: index of the right element
+ * @size: size of the array
+ *
+ * Return: the index of the piv element
+ */
+int partition(int *array, int left, int right, size_t size)
+{
+	int tmp, pivot = array[right];
+	size_t i, j;
+
+	i = left - 1;
+	j = right + 1;
+	while (1)
+	{
+		do {
+			i++;
+		} while (array[i] < pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
+		if (i >= j)
+			return (i);
+		if (i != j)
+		{
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			print_array(array, size);
+		}
+	}
+	return (0);
 }
